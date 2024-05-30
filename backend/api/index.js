d690a5f463a920007e896ce62ad5bfe05c6ca48c
arunkjojo <arunkjojo@gmail.com>
2024-05-30 17:35:27 +0530

    add vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,16 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-const blogRoute = require("./routers/blogRouter");
-const errorHandler = require("./middlewares/errorHandler");
-const corsOptions = require("./utils/cors");
+import app from "../src/app";
 
-const app = express();
-const port = 5000;
-
-app.use(cors(corsOptions));
-app.use("/api/blog", blogRoute);
-app.use(errorHandler);
-
-app.listen(port, () =>
-  console.log(`Server Listening: http://localhost:${port}`)
-);
+export default app;
