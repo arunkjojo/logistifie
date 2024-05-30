0946e4ec49512abedbaa583e6ad6c9ae168491c4
arunkjojo <arunkjojo@gmail.com>
2024-05-30 12:28:37 +0530

    remove errorInfo from errorBoundary

--- front-end/components/ErrorBoundary.tsx
+++ front-end/components/ErrorBoundary.tsx
@@ -37,8 +37,6 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
                     <h2>Something went wrong.</h2>
                     <details style={{ whiteSpace: 'pre-wrap' }}>
                         {this.state.error && this.state.error.toString()}
-                        <br />
-                        {this.state?.errorInfo?.componentStack}
                     </details>
                 </div>
             );
